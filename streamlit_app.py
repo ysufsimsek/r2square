255a3fc582053e23a44e38cc87fde03e5297f60a
ysufsimsek <147537382+ysufsimsek@users.noreply.github.com>
2024-06-06 16:22:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,110 +1,36 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
+import joblib
 
-st.balloons()
-st.markdown("# Data Evaluation App")
+# Başlık ekleyelim
+st.title("R2 Kare Dönem İçi Projesi")
 
-st.write("We are so glad to see you here. ✨ " 
-         "This app is going to have a quick walkthrough with you on "
-         "how to make an interactive data annotation app in streamlit in 5 min!")
+# Modeli ve scaler'ı yükleyelim
+model = joblib.load('stacking_model.pkl')
+scaler = joblib.load('scaler.pkl')
 
-st.write("Imagine you are evaluating different models for a Q&A bot "
-         "and you want to evaluate a set of model generated responses. "
-        "You have collected some user data. "
-         "Here is a sample question and response set.")
-
-data = {
-    "Questions": 
-        ["Who invented the internet?"
-        , "What causes the Northern Lights?"
-        , "Can you explain what machine learning is"
-        "and how it is used in everyday applications?"
-        , "How do penguins fly?"
-    ],           
-    "Answers": 
-        ["The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting" 
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds."
-    ]
+# Kullanıcıdan giriş verisi alalım
+st.header("İstenilen İstatistikleri Giriniz")
+input_data = {
+    'Topla Oynama': st.number_input('Topla Oynama', min_value=0, max_value=100, value=50),
+    'İsabetli Şut': st.number_input('İsabetli Şut', min_value=0, max_value=100, value=10),
+    'Başarılı Paslar': st.number_input('Başarılı Paslar', min_value=0, max_value=1000, value=30),
+    'Korner': st.number_input('Korner', min_value=0, max_value=100, value=5),
+    'Başarılı Orta': st.number_input('Başarılı Orta', min_value=0, max_value=100, value=15)
 }
 
-df = pd.DataFrame(data)
+# Girdi verisini DataFrame'e dönüştürelim
+input_df = pd.DataFrame([input_data])
 
-st.write(df)
+# Veriyi standartlaştıralım
+input_df_scaled = scaler.transform(input_df)
 
-st.write("Now I want to evaluate the responses from my model. "
-         "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-         "You will now notice our dataframe is in the editing mode and try to "
-         "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇")
+# Tahmin yapalım
+if st.button('Tahmin Yap'):
+    prediction = model.predict(input_df_scaled)
+    if input_df['İsabetli Şut'][0] == 0:
+        prediction[0] = 0
+    st.write(f"Tahmin Sonucu: {prediction[0]}")
 
-df["Issue"] = [True, True, True, False]
-df['Category'] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config = {
-        "Questions":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Answers":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Issue":st.column_config.CheckboxColumn(
-            "Mark as annotated?",
-            default = False
-        ),
-        "Category":st.column_config.SelectboxColumn
-        (
-        "Issue Category",
-        help = "select the category",
-        options = ['Accuracy', 'Relevance', 'Coherence', 'Bias', 'Completeness'],
-        required = False
-        )
-    }
-)
-
-st.write("You will notice that we changed our dataframe and added new data. "
-         "Now it is time to visualize what we have annotated!")
-
-st.divider()
-
-st.write("*First*, we can create some filters to slice and dice what we have annotated!")
-
-col1, col2 = st.columns([1,1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options = new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox("Choose a category", options  = new_df[new_df["Issue"]==issue_filter].Category.unique())
-
-st.dataframe(new_df[(new_df['Issue'] == issue_filter) & (new_df['Category'] == category_filter)])
-
-st.markdown("")
-st.write("*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`")
-
-issue_cnt = len(new_df[new_df['Issue']==True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
-
-col1, col2 = st.columns([1,1])
-with col1:
-    st.metric("Number of responses",issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
-
-df_plot = new_df[new_df['Category']!=''].Category.value_counts().reset_index()
-
-st.bar_chart(df_plot, x = 'Category', y = 'count')
-
-st.write("Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:")
-
+# Sayfanın sonuna açıklama ekleyelim
+st.text("Bu uygulama Streamlit kullanılarak oluşturulmuştur.")
