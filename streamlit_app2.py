4a332b4960eb11153e204289aeb2124b6eb6a795
ysufsimsek <147537382+ysufsimsek@users.noreply.github.com>
2024-06-11 02:06:48 +0300

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import webbrowser
 
 st.sidebar.title("Sayfa Seçimi")
-page = st.sidebar.selectbox("", ["Hakkımızda", "Proje"])
+page = st.sidebar.selectbox("", ["Proje Ekip Üyeleri", "Proje"])
 
 if page == "Proje Ekip Üyeleri":
-    st.title("HAKKIMIZDA")
-    st.header("Proje Ekip Üyeleri")
+    st.title("Proje Ekip Üyeleri")
     st.write("R2 Kare Projesinin ekip üyeleri:")
     st.write("**Yusuf ŞİMŞEK**: Fırat Üniversitesi Yapay Zeka ve Veri Mühendisliği 1.sınıf öğrencisi")
     st.write("**Mehmet AYDEMİR**: Fırat Üniversitesi Yapay Zeka ve Veri Mühendisliği 1.sınıf öğrencisi")
@@ -17,34 +15,24 @@ if page == "Proje Ekip Üyeleri":
     st.write("Aşağıda ekip üyelerimizin GitHub ve LinkedIn hesapları bulunmaktadır")
     with st.expander("Ekip Üyeleri GitHub ve LinkedIn Bilgileri"):
         st.write("**Ekip Lideri**: Yusuf ŞİMŞEK")
-        if st.button('LinkedIn - Yusuf ŞİMŞEK'):
-            webbrowser.open_new_tab("https://www.linkedin.com/in/yusuf-%C5%9Fim%C5%9Fek-ab1486295/")
-        if st.button('GitHub - Yusuf ŞİMŞEK'):
-            webbrowser.open_new_tab("https://github.com/ysufsimsek")
+        st.markdown('<a href="https://www.linkedin.com/in/yusuf-%C5%9Fim%C5%9Fek-ab1486295/" target="_blank"><button>LinkedIn - Yusuf ŞİMŞEK</button></a>', unsafe_allow_html=True)
+        st.markdown('<a href="https://github.com/ysufsimsek" target="_blank"><button>GitHub - Yusuf ŞİMŞEK</button></a>', unsafe_allow_html=True)
 
         st.write("**Ekip Üyesi**: Mehmet AYDEMİR")
-        if st.button('LinkedIn - Mehmet AYDEMİR'):
-            webbrowser.open_new_tab("https://www.linkedin.com/in/mehmet-aydemir-7514262a5/")
-        if st.button('GitHub - Mehmet AYDEMİR'):
-            webbrowser.open_new_tab("https://github.com/mehmetaydemir")
+        st.markdown('<a href="https://www.linkedin.com/in/mehmet-aydemir-7514262a5/" target="_blank"><button>LinkedIn - Mehmet AYDEMİR</button></a>', unsafe_allow_html=True)
+        st.markdown('<a href="https://github.com/mehmetaydemir" target="_blank"><button>GitHub - Mehmet AYDEMİR</button></a>', unsafe_allow_html=True)
 
         st.write("**Ekip Üyesi**: Efe Batın SEÇKİN")
-        if st.button('LinkedIn - Efe Batın SEÇKİN'):
-            webbrowser.open_new_tab("https://www.linkedin.com/in/efe-bat%C4%B1n-se%C3%A7kin-b78692295/")
-        if st.button('GitHub - Efe Batın SEÇKİN'):
-            webbrowser.open_new_tab("https://github.com/efebatinseckin")
-
-    st.header("Proje Hakkında Bilgiler")
-    st.write("Veri bilimi için programlaya giriş adlı dersimizin dönem içi proje ödevidir. 14/03/2024 tarihinde projeye başlanılmıştır. Proje belirenen bir takımın atabileceği gol sayısını tahmin etme temeli olan makine öğrenmesi projesidir.Kullanırken iyi eğleceler dileriz. ")
-
+        st.markdown('<a href="https://www.linkedin.com/in/efe-bat%C4%B1n-se%C3%A7kin-b78692295/" target="_blank"><button>LinkedIn - Efe Batın SEÇKİN</button></a>', unsafe_allow_html=True)
+        st.markdown('<a href="https://github.com/EfeSeckinn" target="_blank"><button>GitHub - Efe Batın SEÇKİN</button></a>', unsafe_allow_html=True)
 
 elif page == "Proje":
     st.title("R2 Kare Dönem İçi Projesi")
 
     # Model ve scaler dosyalarının yüklendiğinden emin olun
     try:
-        model = joblib.load('stacking_model.joblib')
-        scaler = joblib.load('scaler.joblib')
+        model = joblib.load('stacking_model.pkl')
+        scaler = joblib.load('scaler.pkl')
     except Exception as e:
         st.error(f"Model veya scaler yüklenirken bir hata oluştu: {e}")
         st.stop()
@@ -66,11 +54,11 @@ elif page == "Proje":
         st.error(f"Veri ölçeklendirilirken bir hata oluştu: {e}")
         st.stop()
 
-    if st.button('Atalıcak Golü tahmin edin'):
+    if st.button('Atılacak Golü tahmin edin'):
         try:
             prediction = model.predict(input_df_scaled)
             if input_df['İsabetli Şut'][0] == 0:
                 prediction[0] = 0
-            st.write(f"Tahmini Barcelonanın Atacağı Gol: {prediction[0]}")
+            st.write(f"Tahmini Barcelona'nın Atacağı Gol: {prediction[0]}")
         except Exception as e:
             st.error(f"Tahmin yapılırken bir hata oluştu: {e}")
